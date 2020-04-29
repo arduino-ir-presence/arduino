2e24f71c602e88b648d3e40344997f1bf0c83966
sujitv19196 <42523808+sujitv19196@users.noreply.github.com>
2020-04-29 16:32:40 -0500

    Add files via upload

---
+++ node_mcu_code.ino
@@ -0,0 +1,48 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+
+int ledPin = 13;                // LED 
+int pirPin = 14;                 // PIR Out pin 
+int pirStat = 0;                   // PIR status
+
+const char* ssid = "theknees"; 
+const char* pass = "kneetime";
+const char* url = "http://192.168.86.34:8000/";
+    
+void setup() {
+ pinMode(LED_BUILTIN, OUTPUT);     
+ pinMode(pirPin, INPUT);     
+ Serial.begin(115200);
+
+ WiFi.begin(ssid, pass);   //WiFi connection
+ 
+ while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
+
+  delay(500);
+  Serial.println("Waiting for connection");
+
+ }
+}
+void loop(){
+ pirStat = digitalRead(pirPin); 
+ if (pirStat == HIGH) {            // if motion detected
+   digitalWrite(LED_BUILTIN, HIGH);  // turn LED ON
+
+   if(WiFi.status()== WL_CONNECTED) {   //Check WiFi connection status
+     HTTPClient http;    //Declare object of class HTTPClient
+     http.begin(url);      //Specify request destination
+     http.addHeader("Content-Type", "text/plain"); 
+     int httpCode = http.POST("TEST");   
+     String payload = http.getString();     
+     Serial.println(httpCode);   //Print HTTP return code
+     Serial.println(payload);    //Print request response payload//Get the response payload
+     http.end();  //Close connection
+   } else {
+      Serial.println("Error in WiFi connection");   
+   }
+
+ } 
+ else { 
+   digitalWrite(LED_BUILTIN, LOW); // turn LED OFF if we have no motion
+ }
+} 
