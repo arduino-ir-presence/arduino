0e800c5b17c1ad4dc37223a7dd31e0daa3fdb38b
sujitv19196 <42523808+sujitv19196@users.noreply.github.com>
2020-04-30 00:59:48 -0500

    Update node_mcu_code.ino

--- node_mcu_code.ino
+++ node_mcu_code.ino
@@ -1,13 +1,11 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include "/Users/sujitvaradhan/arduino_ir_presence/info.h" 
 
-int ledPin = LED_BUILTIN;                // LED 
-int pirPin = 14;                    // PIR IN pin 
+int pirPin = 2;                 // PIR Out pin 
 int pirStat = 0;                   // PIR status
 
-const char* ssid = ""; 
-const char* pass = "";
-const char* url = "";
+
     
 void setup() {
  pinMode(LED_BUILTIN, OUTPUT);     
@@ -25,14 +23,15 @@ void setup() {
 }
 void loop(){
  pirStat = digitalRead(pirPin); 
+// Serial.println(secret);
  if (pirStat == HIGH) {            // if motion detected
    digitalWrite(LED_BUILTIN, HIGH);  // turn LED ON
-
+   delay(1000);
    if(WiFi.status()== WL_CONNECTED) {   //Check WiFi connection status
      HTTPClient http;    //Declare object of class HTTPClient
      http.begin(url);      //Specify request destination
-     http.addHeader("Content-Type", "text/plain"); 
-     int httpCode = http.POST("TEST");   
+//     http.addHeader("Content-Type", "text/plain"); 
+     int httpCode = http.POST("secretId=" + secret + "&isOccupied=true");   
      String payload = http.getString();     
      Serial.println(httpCode);   //Print HTTP return code
      Serial.println(payload);    //Print request response payload//Get the response payload
@@ -44,5 +43,6 @@ void loop(){
  } 
  else { 
    digitalWrite(LED_BUILTIN, LOW); // turn LED OFF if we have no motion
+   Serial.println("here");   
  }
 } 
